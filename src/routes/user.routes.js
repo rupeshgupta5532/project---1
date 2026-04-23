67492cf839c2100591a3800f46095c0cd5effe35
Rupesh Gupta <rupesh.gupta@appinventiv.com>
2026-04-23 11:53:02 +0530

    removed the swagger docs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,94 +2,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: User management APIs
- */
-
-/**
- * @swagger
- * /api/users:
- *   post:
- *     summary: Create a new user
- *     tags: [Users]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *               email:
- *                 type: string
- *               age:
- *                 type: number
- *     responses:
- *       201:
- *         description: User created
- */
 router.post('/', userController.createUser);
 
-/**
- * @swagger
- * /api/users:
- *   get:
- *     summary: Get all users
- *     tags: [Users]
- *     responses:
- *       200:
- *         description: List of users
- */
 router.get('/', userController.getUsers);
 
-/**
- * @swagger
- * /api/users/{id}:
- *   get:
- *     summary: Get user by ID
- *     tags: [Users]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: User found
- */
 router.get('/:id', userController.getUser);
 
-/**
- * @swagger
- * /api/users/{id}:
- *   put:
- *     summary: Update user
- *     tags: [Users]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *     responses:
- *       200:
- *         description: Updated user
- */
 router.put('/:id', userController.updateUser);
 
-/**
- * @swagger
- * /api/users/{id}:
- *   delete:
- *     summary: Delete user
- *     tags: [Users]
- *     responses:
- *       200:
- *         description: User deleted
- */
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
